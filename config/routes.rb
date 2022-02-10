02dcf85b04fb4d9a96dcf75f74e95971b1ac8f0f
Gabriel Rocha <gabrielras12@hotmail.com>
2022-02-10 16:28:48 -0300

    ✨ Add Redis to Sidekiq.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount ClientAPI::BaseAPI => '/api'
+  mount Sidekiq::Web => '/sidekiq'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
